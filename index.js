8f54517eb8bf82622bac8bd120c516a3dce1b014
kickermeister <weitnauer@irt.de>
2016-01-27 09:21:50 +0100

    Adding the removed first time logic again to prevent multiple writing to file during watchify

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var through = require('through')
 var git = require('git-rev-sync')
 var GIT_VERSION = git.short() + " " + git.tag()
 
+var firstTime = true
+
 module.exports = function (bundle) {
   var stream = through(write, end)
 
   return stream
 
   function write(buf) {
-    var notification = '\n'+[
-      'global.__BROWSERIFY_META_DATA__GIT_VERSION = "' + GIT_VERSION + '";',
-      'global.__BROWSERIFY_META_DATA__CREATED_AT = "' + new Date() + '";'
-    ].join('\n')+'\n\n'
-    stream.queue(notification)
+    if (firstTime) {
+      var notification = '\n'+[
+        'global.__BROWSERIFY_META_DATA__GIT_VERSION = "' + GIT_VERSION + '";',
+        'global.__BROWSERIFY_META_DATA__CREATED_AT = "' + new Date() + '";'
+      ].join('\n')+'\n\n'
+      stream.queue(notification)
+      firstTime = false
+    }
     stream.queue(buf)
   }
   function end() {
